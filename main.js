6f8e35444e0d24a0888f24f93a66be2b1be5fc6f
samuelhpratt <samuelhpratt@gmail.com>
2025-07-01 01:56:56 +1000

    Added TODO list

---
+++ main.js
@@ -0,0 +1,26 @@
+/*  TODO
+load gfx files
+load pal files
+draw gfx to screen with palettes
+list of sprites and their locations within the gfx files
+select which sprites you want to include
+detect conflicts
+create exgfx based on selection
+save exgfx file
+
+
+search bar
+categories - castle, koopas, powerups, etc
+sprite images
+"passable" conflict detection (pokey head football, bone spiny etc)
+    show all animation frames, variations etc
+checkbox for lava particles
+
+
+remap sprite locations? (for use with STEAR etc)
+custom sprites?
+
+*/
+
+
+
